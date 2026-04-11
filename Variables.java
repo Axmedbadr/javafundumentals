96497ad42eabb4376df133787ac1532dac6daad1
axmed badri <badri736@hotmail.com>
2026-04-11 21:24:44 +0300

    variables

---
+++ Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+    
+    public static void main(String[]orgs) {
+
+
+     
+     int studentid;
+     int fucutlyid;
+     int semeterid;
+     int amount ;
+
+
+
+      studentid = 01;
+        fucutlyid=02;
+        semeterid=05;
+        amount = 150;
+        
+
+
+     
+
+       System.out.println("student id is" + studentid + "and the fuculty id is" +fucutlyid +  "and the semestier id" +semeterid + "and fee is" + amount);
+        
+
+    }
+}
